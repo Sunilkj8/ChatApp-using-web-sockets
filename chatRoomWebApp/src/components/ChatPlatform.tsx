3274eb53af4b2332d3538f5bb1d803531c3a3384
unknown <sunil2003jakhar@gmail.com>
2025-10-27 11:53:59 +0530

    User count and join feature added

--- chatRoomWebApp/src/components/ChatPlatform.tsx
+++ chatRoomWebApp/src/components/ChatPlatform.tsx
@@ -5,10 +5,18 @@ const ChatPlatform = () => {
   const [currMessage, setCurrMessage] = useState("");
   const [msg, setMsg] = useState([]);
   const [sentMsg, setSentMsg] = useState([]);
-
+  const [isJoined, setIsJoined] = useState(false);
+  const [userCount, setUserCount] = useState("0");
   console.log(currMessage);
   console.log(msg);
   console.log(sentMsg);
+
+  const handleKeyDown = (event: any) => {
+    if (event.key == "Enter") {
+      // console.log("hi there");
+      connectToSocket();
+    }
+  };
 
   function connectToSocket() {
     if (!socket) return;
@@ -47,16 +55,42 @@ const ChatPlatform = () => {
     setSocket(ws);
     ws.onmessage = (ev) => {
       //@ts-ignore
-      setMsg((prevVal) => {
-        return [...prevVal, ev.data];
-      });
+      console.log(ev.data);
+      const joinJsonMessage = JSON.parse(ev.data);
+      console.log(joinJsonMessage);
+      if (joinJsonMessage.users) {
+        setIsJoined(true);
+        setUserCount(joinJsonMessage.users);
+      }
+      //@ts-ignore
+      // else
+      //   setMsg((prevVal) => {
+      //     return [...prevVal, ev.data];
+      //   });
     };
   }, []);
 
   return (
-    <div className="flex gap-5 flex-col relative bg-black justify-center items-center   h-screen w-screen">
-      <div className="border h-[80%] flex flex-col rounded-md w-[35%] bg-black border-gray-300 overflow-hidden">
-        <div className="flex h-[90%] w-full">
+    <div className="flex font-mono gap-5 flex-col relative bg-black justify-center items-center   h-screen w-screen">
+      <div className="border-gray-500 shadow-sm shadow-gray-400 gap-5  h-[80%] flex flex-col rounded-md w-[35%] bg-black  p-3 ">
+        <div className="h-[10%]   flex border-white">
+          <div className="text-white p-1 px-2 text-sm flex items-center w-[70%]">
+            {isJoined ? "Room Joined!" : "Join The Room To Send Messages"}
+          </div>
+          <div className="text-white w-[30%]">
+            {!isJoined ? (
+              <div
+                className="p-2 bg-white text-black rounded-md flex items-center justify-center cursor-pointer"
+                onClick={joinRoom}
+              >
+                Join
+              </div>
+            ) : (
+              <div>Users Connected:{userCount}</div>
+            )}
+          </div>
+        </div>
+        <div className="flex border overflow-y-auto border-gray-500 shadow-sm shadow-gray-400  rounded-xl h-[80%] w-full">
           <div className="w-1/2 items-start   flex flex-col-reverse gap-1 p-2 ">
             {msg.map((elem) => {
               return (
@@ -76,11 +110,12 @@ const ChatPlatform = () => {
             })}
           </div>
         </div>
-        <div className="flex   h-[10%] p-1 justify-between border-white">
+        <div className="flex h-[10%] p-1 justify-between border-white">
           <input
             value={currMessage}
             type="text"
             placeholder="Type Your Message..."
+            onKeyDown={handleKeyDown}
             className="outline-none   w-[80%] text-white    h-full p-1 px-2 "
             onChange={(e) => {
               const { value } = e.target;
@@ -91,17 +126,11 @@ const ChatPlatform = () => {
           />
           <div
             onClick={connectToSocket}
-            className="border py-1 px-2 cursor-pointer w-[20%] text-center flex items-center justify-center  bg-white duration-200 rounded-md"
+            className="border py-1 px-2 cursor-pointer w-[20%] text-center flex items-center justify-center  bg-white duration-200 rounded-md select-none"
           >
             Send
           </div>
         </div>
-      </div>
-      <div
-        onClick={joinRoom}
-        className="absolute right-80 bottom-17 border py-1 px-2 cursor-pointer hover:bg-amber-100 duration-200 rounded-sm"
-      >
-        Join
       </div>
     </div>
   );
