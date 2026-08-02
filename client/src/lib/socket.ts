80c7e38054f955e05caef3e7a4958b71cdfda943
Sunilkj8 <sunil2003jakhar@gmail.com>
2026-08-02 17:26:12 +0530

    chore: update backend URL to production

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'https://sniz.onrender.com';
 
 export const socket: Socket = io(URL, {
   autoConnect: false,
